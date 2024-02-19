58e76d128d1774b4c05aadcf7dd50f847b58d6ee
Joaquin Cespedes <jcespedeschandia@gmail.com>
2024-02-18 22:53:28 -0300

    Se sube Practice 'Hola, mundo'

--- Python/hello_world.py
+++ Python/hello_world.py
@@ -1,5 +1,15 @@
-x = 'Hola. Python'
-print(x)
-y = 42
-print(y)
-
+# 1. Tarea: imprime "hola mundo"
+print('Hola, mundo')
+#2. imprime "Hola, Noelle"
+nombre = 'Joaquin'
+print("Hola,", nombre)
+print('Hola, ' + nombre)
+#3. imprimir "Hola 42!" con el numero en una variable.
+val = 42
+print("Hola", val,'!')
+print("Hola "+str(val)+'!') #BONUS NINJA
+# 4. imprimir "Amo comer sushi y pizza" con las comidad en variables
+plato1 = 'sushi'
+plato2 = 'pizza'
+print('Amo comer {} y {}'.format(plato1,plato2))
+print(f"Amo comer {plato1} y {plato2}")
